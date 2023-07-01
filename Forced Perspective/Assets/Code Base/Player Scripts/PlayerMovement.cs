dd83f4be3b28cc11c574b367bac7f6ce393e3be4
Serhii Karpenko <SerhiiKarpenko@users.noreply.github.com>
2023-07-01 22:57:45 +0200

    Added simple player movement
    Now player can move with WASD

---
+++ Forced Perspective/Assets/Code Base/Player Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Code_Base.Player_Scripts
+{
+    [RequireComponent(typeof(CharacterController))]
+    public class PlayerMovement : MonoBehaviour
+    {
+        [SerializeField] private float _speed = 20;
+        
+        private Vector2 _axis;
+        private CharacterController _characterController;
+
+        private void Start() => 
+            _characterController = GetComponent<CharacterController>();
+
+        private void Update()
+        {
+            _axis.x = Input.GetAxis("Horizontal");
+            _axis.y = Input.GetAxis("Vertical");
+
+            Vector3 movementDirection = transform.right * _axis.x + transform.forward * _axis.y;
+
+            _characterController.Move(movementDirection * _speed * Time.deltaTime);
+        }
+    }
+}
